32e1aaf9f2d4f4a256245fa98379e54a6e17d983
yuvraj9 <shekhawatyuvraj1998@gmail.com>
2021-07-25 18:36:02 +0530

    adding tests

---
+++ test.py
@@ -0,0 +1,22 @@
+from click.testing import CliRunner
+from tests.cli import commands
+
+def test_get_not_found():
+    runner = CliRunner()
+    result = runner.invoke(commands, ['get', 'lamsbda'])
+    assert result.exit_code == 0
+    assert result.output == '{"status_code": 404, "error": "Key Doesn\'t exist"}\n\n'
+
+def test_put_value():
+    runner = CliRunner()
+    testPut = runner.invoke(commands, ['put', 'key1', 'value1'])
+    assert testPut.exit_code == 0
+    assert testPut.output == '{"key1": "value1"}\n\n'
+
+def test_get_value():
+    runner = CliRunner()
+    testGet = runner.invoke(commands, ['get', 'key1'])
+    assert testGet.exit_code == 0
+    assert testGet.output == '{"value": "value1"}\n\n'
+
+# File test
