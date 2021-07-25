a47645a1ecf1e68443c1f830faf8e5091a62284a
yuvraj9 <shekhawatyuvraj1998@gmail.com>
2021-07-25 20:54:02 +0530

    wrong test case remove

--- test.py
+++ test.py
@@ -38,22 +38,3 @@ def test_get_value():
     testGet = runner.invoke(commands, ['get', 'key1'])
     assert testGet.exit_code == 0
     assert testGet.output == '{"value": "value1"}\n\n'
-    verify_from_file()
-
-
-def verify_from_file():
-    """
-    This function verifies if key exist in storage as well. It directly
-    pulls the value of key which we added in test_put_value() function.
-    Then we validate it.
-    """
-    with open('store.json', 'r') as file:
-        data = file.read()
-
-    # Converts file data into a json
-    KVSTORE = json.loads(data)
-
-    # Check for given key in the object and fetches value
-    value = KVSTORE.get('key1')
-
-    assert value == 'value1'
